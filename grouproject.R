f0a5b5290df555c501a7f42cab682415f1f6d078
Tom Tremerel <156415815+TomTremerel@users.noreply.github.com>
2025-04-04 16:27:48 +0200

    Update grouproject.R

--- grouproject.R
+++ grouproject.R
@@ -1,2 +1,24 @@
-  library(tidyverse)
-data <- read.delim("Cusine-pref-raw.txt")  
+library(tidyverse)
+
+library(dplyr)      # data manipulation
+library(cluster)    # clustering algorithms
+library(factoextra) # clustering algorithms and funny visualization
+library(dendextend) # for comparing two dendrograms
+library(psych)
+
+data <- read.delim("Cusine-pref-raw.txt")
+data_num <-data[2:25]
+apply(data_num,1,var)
+scaled_data_num <- scale(data_num)
+apply(scaled_data_num,1,var)
+head(scaled_data_num)
+
+d <- dist(scaled_data_num, method = "euclidean")
+fviz_dist(d, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+hc <- hclust(d, method = "complete" )
+plot(hc, cex = 0.6, hang = -1)
+plot(as.dendrogram(hc), main = "my results")
+
+fviz_nbclust(scaled_data_num, FUN = hcut, method = "wss")
+fviz_nbclust(scaled_data_num, kmeans, method = "silhouette")
+
